92001384ca7bc66b08447c1f97efe5399fb64538
T S <taneymail@gmail.com>
2020-02-16 03:20:58 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,5 +5,31 @@
 
 require 'pry'
 
+class EmailAddressParser
+  attr_accessor :EmailAddressParser
+  @@all = []
+end
 
+def intitializer
+  @EmailAddressParser =  EmailAddressParser
+  @@all = self
+end
+
+def EmailAddressParser = 
+  ("john@doe.com") = ("person@someplace.org")
+  parser = EmailAddressParser.new(email_addresses)
+
+  parser.parse 
+
+end
+
+def self.all 
+  @@all
+end
+
+end
+
+
+
+parser = parser
 
